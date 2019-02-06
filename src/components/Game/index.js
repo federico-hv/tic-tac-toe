fe3dae7dcd71e137ffe52f5c2f8b8da3932f3dd3
Federico Hernández Velásquez <federicohernandez.ve@gmail.com>
2019-02-05 23:37:01 -0300

    Completed tests. Styles to be added.

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -16,7 +16,8 @@ class Game extends Component {
     data: (new Array(3)).fill().map(row => new Array(3).fill('')),
     starterPiece: null,
     player: 0,
-    gameEnded: false
+    gameEnded: false,
+    result: ''
   };
 
   selectPiece = (p) => this.setState({starterPiece: p})
@@ -29,39 +30,111 @@ class Game extends Component {
    */
   onPlayerMove = (pos) => {
     const { data, player, starterPiece } = this.state;
-    data[pos[0]][pos[1]] = player === starterPiece ? 'X' : '0';
+    data[pos[0]][pos[1]] = player === starterPiece ? 'X' : 'O';
     
-    const newPlayer = this.state.player === 0 ? 1 : 0;
+    this.setState({ data }, this.checkGameResult);
+  }
 
-    this.setState({ player: newPlayer, data }, this.checkGameResult);
+  // There is a victory if:
+  // All indexes have same value
+  // A row is complete
+  // Cross [0][0] [1][1] [2][2] or [0][2] [1][1] [2][0] 
+  //
+  // There is a tie if all squares are filled and none of
+  // the above happened
+
+
+  /**
+   * Receives the data array and returns the final result
+   * of the game.
+   * 
+   * 0 if game hasn't finished yet
+   * 1 current player wins
+   * 2 for tie
+   *
+   * @return  {number}  current status of the game
+   */
+  getGameResult = (data) => {
+    let result = 0, missing = false, aux1 = [], aux2 = [], aux3 =[];
+
+    data.every(row => {
+      if (row.every(v => v === row[0] && v !== '')){
+        result = 1;
+        return false
+      }
+
+      if (row.some(v => v === '')) {
+        missing = true;
+      }
+
+      aux1.push(row[0]);
+      aux2.push(row[1]);
+      aux3.push(row[2]);
+      return true;
+    })
+
+    if (result === 1) return result;
+    
+    if (aux1.every( v => v === aux1[0] && v !== '') || 
+        aux2.every( v => v === aux2[0] && v !== '') || 
+        aux3.every( v => v === aux3[0] && v !== ''))
+      return 1;
+
+    if ((data[0][0] === data[1][1] && data[1][1] === data[2][2] && data[0][0] !== '') || 
+        data[0][2] === data[1][1] &&  data[1][1] === data [2][0] && data[0][2] !== '') 
+        return 1;
+
+    if (!missing) return 2;
+
+    return 0;
   }
 
   /**
-   * Called after every player move to analize if the game
-   * has ended either with a victory or a tie. When the game
-   * ends a message with the result is displayed on the screen.
+   * Checks and updates the state of the game
+   * after every move
    */
   checkGameResult = () => {
-    // console.log('CHECKING FINAL RESULT');
-    // There is a victory if:
-    // All indexes have same value
-    // A row is complete
-    // Cross [0][0] [1][1] [2][2] or [0][2] [1][1] [2][0] 
+
+    const { data } = this.state;
+
+    const result = this.getGameResult(data);
+    const newPlayer = this.state.player === 0 ? 1 : 0;
+    
+    switch (result) {
+      case 0:
+        return this.setState({ player: newPlayer });
+      case 1:
+        return this.setState({ gameEnded: true, result: `Player ${this.state.player+1} wins`});
+      case 2:
+        return this.setState({ gameEnded: true, result: `There's a tie`});
+
+    }
   }
 
   render() {
     return (
       <div>
-        <h1 className="player-turn">{this.state.player === 0 ? 'First Player' : 'Second Player'}</h1>
-        {this.state.starterPiece !== null ? <Board onMove={this.onPlayerMove} data={this.state.data} /> : 
-          (
-            <div className="piece-selector">
-              Select 
-              <button id="btnX" onClick={()=>this.selectPiece(0)}>x</button>
-              <button id="btnO" onClick={()=>this.selectPiece(1)}>o</button>
+        {
+          this.state.gameEnded ? 
+            (
+              <div className="game-result">{this.state.result}</div>
+            )
+          : (
+            <div className="board">
+              <h1 className="player-turn">{this.state.player === 0 ? 'First Player' : 'Second Player'}</h1>
+              {this.state.starterPiece !== null ? <Board onMove={this.onPlayerMove} data={this.state.data} /> : 
+                (
+                  <div className="piece-selector">
+                    Select 
+                    <button id="btnX" onClick={()=>this.selectPiece(0)}>x</button>
+                    <button id="btnO" onClick={()=>this.selectPiece(1)}>o</button>
+                  </div>
+                )
+              }
             </div>
           )
         }
+        
       </div>
     );
   }
--- src/components/Game/index.test.js
+++ src/components/Game/index.test.js
@@ -6,19 +6,67 @@ import toJson from 'enzyme-to-json';
 
 describe('<Game />', ()=>{
 
-	it('Should render a container div', () => {
+	it('Should render a game container if game started', () => {
 		const wrapper = shallow(<Game />);
-		expect(wrapper.find('div').length).toEqual(2);
+		expect(wrapper.find('.board').length).toEqual(1);
 		wrapper.unmount();
 	});
-
-	it('Should render the turn of the player according to state.turn', () => {	
+	
+	it('Should update game data properly onPlayerMove', () => {	
 		const app = shallow(<Game />);
-		expect(app.find('.player-turn').text()).toEqual('First Player');
+		const data = app.state().data;
+	
+		app.setState({ player: 0, starterPiece: 0 })
+		expect(data).toEqual([ [ '', '', '' ], [ '', '', '' ], [ '', '', '' ] ])
 		app.instance().onPlayerMove([2,2]);
-		expect(app.find('.player-turn').text()).toEqual('Second Player');
-		app.instance().onPlayerMove([0,2]);
+		expect(data).toEqual([ [ '', '', '' ], [ '', '', '' ], [ '', '', 'X' ] ])
+		app.setState({ player: 1 })
+		app.instance().onPlayerMove([1,1]);
+		expect(data).toEqual([ [ '', '', '' ], [ '', 'O', '' ], [ '', '', 'X' ] ])
+	})
+
+	it('Should return the game result', () => {
+		const app = shallow(<Game />);
+		let data = app.state().data;
+		
+		//default data means game is still on
+		expect(app.instance().getGameResult(data)).toEqual(0);
+		
+		// an array with a winning result should return 1
+		data = [['X', '', 'O'],['O','X',''],['X','O','X']]
+		app.setState({ data });
+		expect(app.instance().getGameResult(data)).toEqual(1);
+
+		// an array with a tie result should return 2
+		data = [['X', 'O', 'X'],['O','O','X'],['X','X','O']]
+		app.setState({ data });
+		expect(app.instance().getGameResult(data)).toEqual(2);
+	})
+
+
+	it('Should render the final result after checkGameResult when game ends', () => {
+		const app = shallow(<Game />);
+		const data = [['X', '', 'O'],['X','O',''],['X','O','X']];
+		
+		app.setState({ data });
+		app.instance().getGameResult = jest.fn().mockReturnValueOnce(1);
+		app.instance().checkGameResult();
+		expect(app.find('.board').length).toEqual(0);
+		expect(app.find('.game-result').length).toEqual(1);
+	})
+
+	
+	it('Should render the turn of the corresponding player on every turn', () => {
+		const app = shallow(<Game />);
+		const data = [['X', '', 'O'],['X','O',''],['X','O','X']];
+		
+		expect(app.find('.player-turn').length).toEqual(1);
 		expect(app.find('.player-turn').text()).toEqual('First Player');
+
+		app.instance().getGameResult = jest.fn().mockReturnValueOnce(0);
+		app.instance().checkGameResult();
+		
+		expect(app.find('.player-turn').text()).toEqual('Second Player');
 	})
 
 	it('Should render piece selector before game starts and board after', () => {	
@@ -38,11 +86,30 @@ describe('<Game />', ()=>{
 		expect(toJson(tree)).toMatchSnapshot();
 		tree.unmount();
 	});
-
+	
 });
 
 
-describe('<Piece />', ()=>{
+describe('<Board />', () => {
+
+	it('Should a div with class board', ()=> {
+		const data = [['X', '', 'O'],['O','X',''],['X','O','X']]
+		const board = shallow(<Board data={data} />);
+
+		expect(board.find('.board').length).toEqual(1);
+	})
+
+	it('Matches snapshot', () => {
+		const data = [['X', '', 'O'],['O','X',''],['X','O','X']]
+		const board = shallow(<Board data={data} />);
+		
+		expect(toJson(board)).toMatchSnapshot();
+		board.unmount();
+	});
+})
+
+
+describe('<Piece />', () => {
 
 	it('Should render an empty space', () => {
 		const piece = shallow(<Piece symbol={{ value: null }} />)
